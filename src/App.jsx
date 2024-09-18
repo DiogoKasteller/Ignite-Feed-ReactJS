3dd4b1c2382fdc1aaa9ae3ba4528f36d94b6f3a1
Diogo Kasteller <diogokasteller@gmail.com>
2024-09-18 15:48:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Post } from "./Post.jsx"
+import { Header } from "./components/Header.jsx"
+
+import styles from "./App.module.css"
+import "./global.css"
+import { Sidebar } from "./components/Sidebar.jsx"
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego Fernandes" content="Um novo post muito legal" />
+          <Post author="Diogo Kasteller" content="Um novo post muito legal 2" />
+        </main>
+      </div>
+    </div>
+  )
+}
