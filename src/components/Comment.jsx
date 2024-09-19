67c348feb86ac81a5f2d2306d5fa285cae6cacf3
Diogo Kasteller <diogokasteller@gmail.com>
2024-09-19 14:22:13 -0300

    finishing styling components and responsiveness

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react"
 import styles from "./Comment.module.css"
+import { Avatar } from "./Avatar"
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diogoKasteller.png" />
+      <Avatar hasBorder={false} src="https://github.com/diogoKasteller.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -20,7 +21,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
@@ -30,7 +31,7 @@ export function Comment() {
         <footer>
           <button>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Curtir <span>20</span>
           </button>
         </footer>
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar.jsx"
 
 import styles from "./Sidebar.module.css"
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/diogoKasteller.png"
-        />
+        <Avatar src="https://github.com/diogoKasteller.png" />
 
         <strong>Diogo Kasteller</strong>
         <span>Web Developer</span>
