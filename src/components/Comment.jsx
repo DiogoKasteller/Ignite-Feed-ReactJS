cdf20ec6c96f1a7e2e88d64936913d286dbadcbe
Diogo Kasteller <diogokasteller@gmail.com>
2024-09-20 10:19:42 -0300

    adding delete comment functionality and validating form input

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import styles from "./Comment.module.css"
 import { Avatar } from "./Avatar"
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diogoKasteller.png" />
@@ -20,7 +24,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
@@ -31,7 +35,7 @@ export function Comment({ content }) {
         <footer>
           <button>
             <ThumbsUp />
-            Curtir <span>20</span>
+            Aplaudir <span>20</span>
           </button>
         </footer>
       </div>
